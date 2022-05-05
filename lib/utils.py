aad16beb1e2c420ce581fea90160642ce527d6a5
Raphael Jolivet <raphael.jolivet@mines-paristech.fr>
2022-05-05 17:15:35 +0200

    Cleanup chapter 01. Added common function to fetch data.

---
+++ lib/utils.py
@@ -0,0 +1,95 @@
+import os, sys
+import pandas as pd
+from libinsitu.common import nc2df
+from datetime import datetime, timedelta
+import sg2
+import numpy as np
+import json
+
+
+URL = 'http://tds.webservice-energy.org/thredds/dodsC/surfrad-stations/SURFRAD-BON.nc'
+TMP_FILE="./tmpData/data.parquet"
+META_FILE="./tmpData/data.meta"
+
+class npEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.int32):
+            return int(obj)
+        return json.JSONEncoder.default(self, obj)
+
+def fetch_data() :
+    """ Fetch 3 years of data from OpenDAP or from tmp file"""
+    if os.path.exists(TMP_FILE) :
+        out = pd.read_parquet(TMP_FILE)
+        
+        # Load meta data
+        with open(META_FILE, 'r') as f:
+            attrs = json.load(f)
+        out.attrs = attrs
+        
+        return out
+    
+    
+    start = datetime.now() - timedelta(days=365*3)
+
+    # Load data over OpenDAP into a pandas DataFrame
+    # 'url' is defined in the section above 
+    data = nc2df(URL, vars=["GHI", "DHI", "BNI"], start_time=start, drop_duplicates=True)
+    
+    lon = float(data.attrs["Station_Longitude"])
+    lat = float(data.attrs["Station_Latitude"])
+    elev = float(data.attrs["Station_Elevation"])
+    
+    params = [lon, lat, elev]
+    
+    # Compute sun position 
+    sun_pos = sg2.sun_position(
+        [params], 
+        data.index.values,
+             ["geoc.EOT",
+              "topoc.omega",
+              "topoc.gamma_S0",
+              "topoc.alpha_S",
+              "topoc.delta",
+              "topoc.r_alpha",
+              "topoc.toa_hi",
+              "topoc.toa_ni"])
+    
+    # Compute sunrise / sunset times
+    sun_rise = sg2.sun_rise(
+        [params], 
+        data.index.values)
+    
+    
+    # Combine the two
+    out = pd.DataFrame({
+       'GHI': data.GHI,
+       'BNI': data.BNI,
+       'DHI': data.DHI,
+       'DELTA':np.squeeze(sun_pos.topoc.delta),
+       'OMEGA':np.squeeze(sun_pos.topoc.omega),
+       'EOT':np.squeeze(sun_pos.geoc.EOT),
+       'THETA_Z':np.squeeze(np.pi/2-sun_pos.topoc.gamma_S0),
+       'GAMMA_S0':np.squeeze(sun_pos.topoc.gamma_S0),
+       'ALPHA_S':np.squeeze(sun_pos.topoc.alpha_S),
+       'R':np.squeeze(sun_pos.topoc.r_alpha),
+       'TOA':np.squeeze(sun_pos.topoc.toa_hi),
+       'TOANI':np.squeeze(sun_pos.topoc.toa_ni),
+       'SR_h':sun_rise[:,0,0],
+       'SS_h':sun_rise[:,0,2],
+      }, index=data.index.values)
+    
+    # Copy metadata
+    out.attrs = data.attrs
+    
+    out.to_parquet(TMP_FILE)
+    
+    # Save meta data
+    with open(META_FILE, 'w') as f:
+        json.dump(out.attrs, f, cls=npEncoder)
+    
+    return out
+    
+    
+        
+    
---
+++ lib/wps.py
@@ -0,0 +1,130 @@
+def wps_SG2(location, date_begin, date_end, dt):
+    # location: geopoint [lat lon elev]
+    # date_begin: datetime
+    # date_end: datetime
+    # dt: time step (hour)
+
+    import uuid
+    import os
+    import pandas
+    from datetime import datetime
+    from datetime import timedelta
+    from urllib.request import urlopen
+    import numpy as np
+
+    if (len(location) == 2):
+        location[3] = -999;
+
+    #unique id
+    uid = uuid.uuid4().hex
+
+    fic_output_csv = 'compute_sun_position_output_{}.csv'.format(uid)
+
+    nd = int((date_end-date_begin).total_seconds()/(dt*3600))+1
+
+    str_wps = 'http://toolbox.webservice-energy.org/service/wps?Service=WPS&Request=Execute&Identifier=ComputeSunPosition&version=1.0.0&RawDataOutput=result&DataInputs=';
+    datainputs_wps = 'latitude={:.6f};longitude={:.6f};altitude={:.1f};offset={};count={:d};increment={:.12f}'.format(location[0], location[1], location[2],date_begin.strftime('%Y-%m-%dT%H:%M:%S'),nd, dt/24);
+    
+    response = urlopen('{}{}'.format(str_wps,datainputs_wps))
+    SG2 = pandas.read_csv(response,delimiter=';',comment='#',header=None,names=["JDUT","YYYY","MM","DD","H","DOY","DELTA","OMEGA","GAMMA_S0","ALPHA_S","R"])
+    
+    ### pd.to_datetime(np.round(24*60*SG2.JDUT)/24/60,unit='D',origin='julian')
+    deltatime_min=(np.round((SG2.JDUT[:]).to_numpy()*24*60*60)).astype('timedelta64[s]')
+    time=(np.datetime64('-4713-11-24 12:00')+deltatime_min).astype(datetime)
+    SG2.insert(0,'time',time)
+    
+    offset=1
+    dayofyear = SG2.time.dt.dayofyear
+    day_angle=(2. * np.pi / 365.) * (dayofyear - offset)    
+    eot = (1440.0 / 2 / np.pi) * (
+        0.0000075 +
+        0.001868 * np.cos(day_angle) - 0.032077 * np.sin(day_angle) -
+        0.014615 * np.cos(2.0 * day_angle) - 0.040849 * np.sin(2.0 * day_angle))
+    SG2.insert(9,'EOT',eot)
+    
+    SG2.insert(10,'THETA_Z',np.pi/2-SG2.GAMMA_S0)
+    TOA=np.maximum(1367*(SG2.R**-2)*np.sin(SG2.GAMMA_S0),0)
+    SG2.insert(14,'TOA',TOA)
+    
+    TOANI=np.maximum(1367*(SG2.R**-2),0)
+    SG2.insert(SG2.columns.size,'TOANI',TOANI)
+    
+    # calculation of theroretical the sunset and sunrise
+    lat=location[0]
+    lon=location[1]
+    cos_SRSS=(np.sin(-0.83*np.pi/180)-np.sin(lat*np.pi/180)*np.sin(SG2.DELTA))/(np.cos(lat*np.pi/180)*np.cos(SG2.DELTA))
+    SR_h=12-np.arccos(cos_SRSS)*24/(2*np.pi)+SG2.EOT/60+24*lon/(360)
+    SS_h=12+np.arccos(cos_SRSS)*24/(2*np.pi)+SG2.EOT/60+24*lon/(360)
+    SG2.insert(SG2.columns.size,'SR_h',SR_h)
+    SG2.insert(SG2.columns.size,'SS_h',SS_h)
+
+    SG2.drop(columns='JDUT',inplace=True)
+    SG2.drop(columns='YYYY',inplace=True)
+    SG2.drop(columns='MM',inplace=True)
+    SG2.drop(columns='DD',inplace=True)
+    SG2.drop(columns='H',inplace=True)
+    SG2.drop(columns='DOY',inplace=True)
+    SG2.set_index('time',inplace=True)
+    
+    return SG2
+    
+def wps_CAMS_RAD(location, date_begin, date_end, dt, email):
+    # location: geopoint [lat lon elev]
+    # date_begin: datetime
+    # date_end: datetime
+    # dt: time step (hour)
+    # email: username(email) in soda-pro.com (string)
+
+    import uuid
+    import os
+    import pandas
+    from datetime import datetime
+    from datetime import timedelta
+    from urllib.request import urlopen
+
+    dt60_to_duration = {1: "PT01M", 15: "PT15M", 60: "PT01H", 1440: "P01D"}
+
+    if (len(location) == 2):
+        location[3] = -999;
+
+    #unique id
+    uid = uuid.uuid4().hex
+
+    fic_output_csv = 'get_cams_rad_output_{}.csv'.format(uid)
+
+    str_wps = 'http://www.soda-is.com/service/wps?Service=WPS&Request=Execute&Identifier=get_cams_radiation&version=1.0.0&RawDataOutput=irradiation&DataInputs='
+    datainputs_wps = 'latitude={:.6f};longitude={:.6f};altitude={:.1f};date_begin={};date_end={};time_ref=UT;summarization={};username={}'\
+	.format(location[0], location[1], location[2],date_begin.strftime('%Y-%m-%d'),date_end.strftime('%Y-%m-%d'),dt60_to_duration.get(int(dt*60),"PT60M"),email.replace("@","%2540"));
+    
+    print(datainputs_wps)
+    response = urlopen('{}{}'.format(str_wps,datainputs_wps))
+    #CAMS = pandas.read_csv(response,delimiter='[;,|/]',comment='#',header=None,names=['TOA', 'CLEAR_SKY_GHI', 'CLEAR_SKY_BHI', 'CLEAR_SKY_DHI', 'CLEAR_SKY_BNI', 'GHI', 'BHI','DHI','BNI','Reliability'])
+    #CAMS = pandas.read_csv(response,delimiter='[;,|/]',comment='#',header=None,names=['T1','T2','TOA', 'CLEAR_SKY_GHI', 'CLEAR_SKY_BHI', 'CLEAR_SKY_DHI', 'CLEAR_SKY_BNI', 'GHI', 'BHI','DHI','BNI','Reliability'],engine='python')
+    CAMS = pandas.read_csv(response,delimiter='[;,/]',comment='#',header=None,names=['T1','T2','TOA', 'CLEAR_SKY_GHI', 'CLEAR_SKY_BHI', 'CLEAR_SKY_DHI', 'CLEAR_SKY_BNI', 'GHI', 'BHI','DHI','BNI','Reliability'],engine='python')
+    return CAMS
+    
+def wps_Horizon_SRTM(location):
+    # location: geopoint [lat lon elev]
+
+    import uuid
+    import os
+    import pandas
+    from urllib.request import urlopen
+
+    if (len(location) == 2):
+        location[3] = -999;
+
+    #unique id
+    uid = uuid.uuid4().hex
+
+    fic_output_csv = 'horizon_srtm_output_{}.csv'.format(uid)
+
+    str_wps = 'http://toolbox.webservice-energy.org/service/wps?service=WPS&request=Execute&identifier=compute_horizon_srtm&version=1.0.0&DataInputs=';
+    datainputs_wps = 'latitude={:.6f};longitude={:.6f};altitude={:.1f}'\
+	.format(location[0], location[1], location[2]);
+    
+    print(datainputs_wps)
+    response = urlopen('{}{}'.format(str_wps,datainputs_wps))
+    HZ = pandas.read_csv(response,delimiter=';',comment='#',header=None,skiprows=16,nrows=360,names=['AZIMUT', 'ELEVATION'])
+     
+    return HZ
